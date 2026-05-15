bd686cc41a53f31616bf4cdebffb0578c41a7518
buguno <brunnodesouzabezerra@gmail.com>
2026-05-15 20:28:10 -0300

    feat(provider): changing local backend to s3

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.45.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-807651049620-us-east-1-an"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
