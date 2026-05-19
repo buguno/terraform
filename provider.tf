997181ecb100b07c92df392afa8c9b590dd83f3a
buguno <brunnodesouzabezerra@gmail.com>
2026-05-19 16:31:10 -0300

    feat(provider): adding kubernetes and config provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.45.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.1.0"
+    }
   }
   backend "s3" {
     bucket = "terraform-807651049620-us-east-1-an"
@@ -14,4 +18,14 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
