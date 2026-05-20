58986a17d8068b7b2dbf90845ac9de621f06c6a5
buguno <brunnodesouzabezerra@gmail.com>
2026-05-20 11:33:10 -0300

    feat(output): adding descriptions

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -1,19 +1,24 @@
 output "subnet_pub_1a" {
-  value = aws_subnet.eks_public_subnet_1a.id
+  description = "ID of the public subnet in availability zone us-east-1a."
+  value       = aws_subnet.eks_public_subnet_1a.id
 }
 
 output "subnet_pub_1b" {
-  value = aws_subnet.eks_public_subnet_1b.id
+  description = "ID of the public subnet in availability zone us-east-1b."
+  value       = aws_subnet.eks_public_subnet_1b.id
 }
 
 output "subnet_priv_1a" {
-  value = aws_subnet.eks_private_subnet_1a.id
+  description = "ID of the private subnet in availability zone us-east-1a."
+  value       = aws_subnet.eks_private_subnet_1a.id
 }
 
 output "subnet_priv_1b" {
-  value = aws_subnet.eks_private_subnet_1b.id
+  description = "ID of the private subnet in availability zone us-east-1b."
+  value       = aws_subnet.eks_private_subnet_1b.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.eks_vpc.id
+  description = "ID of the VPC created for the EKS cluster."
+  value       = aws_vpc.eks_vpc.id
 }
