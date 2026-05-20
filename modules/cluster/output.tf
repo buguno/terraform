cd5063acde2f031427450cd7c2fee015a31c93a0
buguno <brunnodesouzabezerra@gmail.com>
2026-05-20 11:32:48 -0300

    feat(output): adding descriptions

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,15 +1,19 @@
 output "cluster_name" {
-  value = aws_eks_cluster.eks_cluster.id
+  description = "The name of the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.id
 }
 
 output "oidc" {
-  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  description = "The OIDC issuer URL of the EKS cluster, used for IAM role federation."
+  value       = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 output "certificate_authority" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+  description = "Base64-encoded certificate authority data for the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
+  description = "The API server endpoint URL of the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.endpoint
 }
