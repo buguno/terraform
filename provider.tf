0b7897c28848bef3134882bee021446bad65af42
buguno <brunnodesouzabezerra@gmail.com>
2026-05-20 14:41:04 -0300

    refactor(provider): removing s3 backend to transform the project in super eks module cluster and changing dynamic region

--- provider.tf
+++ provider.tf
@@ -13,15 +13,10 @@ terraform {
       version = "3.1.1"
     }
   }
-  backend "s3" {
-    bucket = "terraform-807651049620-us-east-1-an"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 provider "kubernetes" {
