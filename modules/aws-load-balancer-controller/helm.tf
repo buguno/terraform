b3b5f112278e8dc92d626f37dd9d3e3f3d7a4338
buguno <brunnodesouzabezerra@gmail.com>
2026-05-20 11:05:37 -0300

    feat(helm): adding helm controller

---
+++ modules/aws-load-balancer-controller/helm.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "eks_helm_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "3.3.0"
+  namespace  = "kube-system"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "region"
+      value = data.aws_region.current.region
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    }
+  ]
+}
